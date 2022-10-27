71dba0c62913cfe70a2bc52c747695910e0608cc
Son Thanh Vo <sonvo95@outlook.com>
2022-10-27 10:34:08 +0200

    refactor: use mutation observer to modify cards

--- public/toggle-content.js
+++ public/toggle-content.js
@@ -2,7 +2,7 @@ const FOOTER_CLASS = ".ghx-card-footer";
 const META_CLASS = ".ghx-extra-fields";
 const HIGHLIGHT_CLASS = ".ghx-highlighted-fields";
 const ISSUE_NUMBER_CLASS = ".ghx-key";
-const AVATAR_CLASS = ".ghx-avatar";
+const AVATAR_CLASS = ".ghx-card-footer > .ghx-avatar";
 const CARD_CLASS = ".ghx-issue";
 
 function toggle_hidden(selector, state) {
@@ -12,11 +12,25 @@ function toggle_hidden(selector, state) {
   });
 }
 
+function hasAvatarInContent(element) {
+  const footer = element.parentNode;
+  const card = footer.parentNode;
+  return Array.from(card.childNodes).reduce(
+    (acc, cur) => acc || cur.classList.contains("ghx-avatar"),
+    false
+  );
+}
+
 function pull_card_out_of_footer(selector) {
   const avatar_element = document.querySelectorAll(selector);
   avatar_element.forEach((element) => {
+    if (hasAvatarInContent(element)) {
+      return;
+    }
+
     const footer = element.parentNode;
     const card = footer.parentNode;
+
     card.appendChild(element);
     card.style.minHeight = "32px";
   });
@@ -41,14 +55,12 @@ function mutate_dom() {
   pull_card_out_of_footer(AVATAR_CLASS);
 }
 
-// initial in mount
-const timer = setInterval(() => {
-  const card = document.querySelector(CARD_CLASS);
-  if (card !== null) {
-    clearInterval(timer);
-    mutate_dom();
-  }
-}, 100);
+// react to dom changes (new cards added and so on)
+const observer = new MutationObserver(mutate_dom);
+observer.observe(document, {
+  childList: true,
+  subtree: true,
+});
 
 // observe storage changes
 chrome.storage.onChanged.addListener(function (changes) {
